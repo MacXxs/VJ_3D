a3034ed226bfc8eedec2bf639c2388b60894b656
MacXxs <mikipmoline@gmail.com>
2018-12-14 19:24:08 +0100

    Càmara reshulona definitiva

--- Assets/CarCamera/CarCam.cs
+++ Assets/CarCamera/CarCam.cs
@@ -2,49 +2,33 @@
 
 public class CarCam : MonoBehaviour
 {
-    Transform rootNode;
+    Transform camTrans;
     Transform carCam;
     Transform car;
-    Rigidbody carPhysics;
+    
+    [Tooltip("Offset de distància de seguiment de la càmara al cotxe, quant menor sigui el valor, més enrere es quedarà")]
+    public float howClose = 10.0f;
 
-    [Tooltip("If car speed is below this value, then the camera will default to looking forwards.")]
-    public float rotationThreshold = 1f;
+    [Tooltip("Offset de distància entre la càmara i el cotxe, quant menor sigui el valor, més aprop del cotxe estarà la càmara")]
+    public float yDistance = 10.0f;
     
-    [Tooltip("How closely the camera follows the car's position. The lower the value, the more the camera will lag behind.")]
-    public float cameraStickiness = 10.0f;
-    
-    [Tooltip("How closely the camera matches the car's velocity vector. The lower the value, the smoother the camera rotations, but too much results in not being able to see where you're going.")]
-    public float cameraRotationSpeed = 5.0f;
 
     void Awake()
     {
         carCam = Camera.main.GetComponent<Transform>();
-        rootNode = GetComponent<Transform>();
-        car = rootNode.parent.GetComponent<Transform>();
-        carPhysics = car.GetComponent<Rigidbody>();
+        camTrans = GetComponent<Transform>();
+        car = camTrans.parent.GetComponent<Transform>();
     }
 
     void Start()
     {
-        // Detach the camera so that it can move freely on its own.
-        rootNode.parent = null;
+        // En cas de que el pare es mori la cam es mou lliurement
+        camTrans.parent = null;
     }
 
     void FixedUpdate()
     {
-        Quaternion look;
-
-        // Moves the camera to match the car's position.
-        rootNode.position = Vector3.Lerp(rootNode.position, car.position, cameraStickiness * Time.fixedDeltaTime);
-
-        // If the car isn't moving, default to looking forwards. Prevents camera from freaking out with a zero velocity getting put into a Quaternion.LookRotation
-        if (carPhysics.velocity.magnitude < rotationThreshold)
-            look = Quaternion.LookRotation(car.forward);
-        else
-            look = Quaternion.LookRotation(carPhysics.velocity.normalized);
-        
-        // Rotate the camera towards the velocity vector.
-        look = Quaternion.Slerp(rootNode.rotation, look, cameraRotationSpeed * Time.fixedDeltaTime);                
-        rootNode.rotation = look;
+        // Es mou la càmara per apropar-se al cotxe
+        camTrans.position = Vector3.Lerp(camTrans.position + new Vector3(0,yDistance,0), car.position, howClose * Time.fixedDeltaTime);
     }
 }
